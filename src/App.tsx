7eb5835aac27cc78fe080ff1d8987c9e166354ce
Axn-Switch <dietespifftimothyswift@gmail.com>
2024-04-05 18:34:34 +0100

    setting up the home page and adding the components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ function App() {
 
   return (
     <>
+      <h1 className="text-5xl text-center">Lamp Post</h1>
+
+      <div id="container">
+        <div className="tile"></div>
+      </div>
     </>
   )
 }
